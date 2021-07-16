ae7cdcd8ef846374c3f6a8d866f665f917962845
1106659799@qq.com <1106659799@qq.com>
2021-07-16 17:15:07 +0800

    修改

--- funcs.go
+++ funcs.go
@@ -25,14 +25,14 @@ func GetHostName() string {
 	return name + "v1.0.2"
 }
 
-func GetLocalIp() string {
+func GetLocalIp() (string, error) {
 	conn, err := net.Dial("udp", "8.8.8.8:53")
 	if err != nil {
 		log.Printf("get local addr err:%v", err)
-		return ""
+		return "", err
 	} else {
 		localIp := strings.Split(conn.LocalAddr().String(), ":")[0]
 		conn.Close()
-		return localIp
+		return localIp, nil
 	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/pizicaiman/cmtools
+module github.com/pizicaiman/cmtools/v2
 
 go 1.16
